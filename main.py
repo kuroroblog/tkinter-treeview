c5166c12b7687c720f7410d6fee5ef375bafa635
tocchimittsu <arimagedon21@gmail.com>
2021-08-04 15:29:29 +0900

    修正

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Application(tk.Frame):
         #############################################
 
         #############################################
-        # <各列の見出しを設定>
+        # <各列の見出しに対してオプション設定>
         # #0 : 階層列(ツリーカラム)を意味する。
         # 階層列(ツリーカラム)の見出しを設定。階層列とする。
         self.treeview.heading("#0", text="階層列")
@@ -60,27 +60,27 @@ class Application(tk.Frame):
         # 第一引数 : 階層化する場合、親要素のアイテムIDを指定。階層化しない場合、""を指定する。
         # 第二引数 : どのindex(アイテム位置)へアイテムを挿入するのか指定する。tk.END : index(アイテムの最終位置)
         # text option : 階層列へ表示するアイテムの名前を設定。
-        # open option : 子要素のアイテムを展開して表示するのかどうか。True : 子要素のアイテムを展開して表示する, False : 子要素のアイテムを展開して表示しない、デフォルト。
-        # value option : データ列へ表示する値を設定。
+        # open option : 子要素のアイテムを展開して表示するのかどうか設定する。True : 子要素のアイテムを展開して表示する, False : 子要素のアイテムを展開して表示しない、デフォルト。
+        # values option : データ列へ表示する値を設定。
         # 戻り値 : アイテムID
         itemAId = self.treeview.insert("", tk.END, text="itemA", open=True, values=("data1a", "data1b", "data1c"))
         # 第一引数 : 階層化する場合、親要素のアイテムIDを指定。階層化しない場合、""を指定する。
         # 第二引数 : どのindex(アイテム位置)へアイテムを挿入するのか指定する。tk.END : index(アイテムの最終位置)
         # text option : 階層列へ表示するアイテムの名前を設定。
-        # open option : 子要素のアイテムを展開して表示するのかどうか。True : 子要素のアイテムを展開して表示する, False : 子要素のアイテムを展開して表示しない、デフォルト。
-        # value option : データ列へ表示する値を設定。
+        # open option : 子要素のアイテムを展開して表示するのかどうか設定する。True : 子要素のアイテムを展開して表示する, False : 子要素のアイテムを展開して表示しない、デフォルト。
+        # values option : データ列へ表示する値を設定。
         # 戻り値 : アイテムID
         itemBId = self.treeview.insert(itemAId, tk.END, text="itemB", open=True, values=("data2a", "data2b", "data2c"))
         # 第一引数 : 階層化する場合、親要素のアイテムIDを指定。階層化しない場合、""を指定する。
         # 第二引数 : どのindex(アイテム位置)へアイテムを挿入するのか指定する。tk.END : index(アイテムの最終位置)
         # text option : 階層列へ表示するアイテムの名前を設定。
-        # value option : データ列へ表示する値を設定。
+        # values option : データ列へ表示する値を設定。
         # 戻り値 : アイテムID
         itemCId = self.treeview.insert(itemBId, tk.END, text="itemC", values=("data3a", "data3b", "data3c"))
         # 第一引数 : 階層化する場合、親要素のアイテムIDを指定。階層化しない場合、""を指定する。
         # 第二引数 : どのindex(アイテム位置)へアイテムを挿入するのか指定する。tk.END : index(アイテムの最終位置)
         # text option : 階層列へ表示するアイテムの名前を設定。
-        # value option : データ列へ表示する値を設定。
+        # values option : データ列へ表示する値を設定。
         # 戻り値 : アイテムID
         itemDId = self.treeview.insert("", tk.END, text="itemD", values=("data4a", "data4b", "data4c"))
         #############################################
